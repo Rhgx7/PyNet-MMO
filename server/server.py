1a0885477b3b866910ccec28314b8091c304cb9d
Rhgxz7 <l14aruben@gmail.com>
2021-10-04 20:14:20 +0100

    Added code

--- server/server.py
+++ server/server.py
@@ -3,5 +3,19 @@ import threading
 
 PORT = 7070 #port for testing purposes only
 HOST = socket.gethostbyname(socket.gethostname())
+ADDR = (HOST, PORT)
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+def handle_client(conn, addr):
+    pass
+
+def start():
+    server.listen()
+    while True:
+        conn, addr = server.accept()
+
+
+print("Starting server")
+start()
